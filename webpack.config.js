02632333dede6976c8991725fbc195d9d5ea1dbd
chenyaqi <henchenyaqi319@gmail.com>
2024-10-05 12:49:40 +0800

    feat: getting started

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+/** @type {import("webpack").Configuration} */
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+}
